ca5acef78023f0224add6afe4cb797e96fcb1543
gngai01 <Gabriel.Ngai@teranet.ca>
2019-01-09 15:59:43 -0500

    Created entities and refactored using Project Lombok
    Change-Id: I9ff1e24c570c29175d129a1b0b0de30bba53eecf

---
+++ src/main/java/com/ngai/springit/domain/Comment.java
@@ -0,0 +1,21 @@
+package com.ngai.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String body;
+
+    // link
+}
